fd86387c6c4f8e4e8bc708fb5408a0904a002242
Winston-Dev <bomwinston@gmail.com>
2024-07-17 22:15:46 -0300

    inserção de regra de negocio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,11 +11,25 @@ public class Dev {
     private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
     public  void increverBootcapm(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getCaonteudos());
+        bootcamp.getDevsInscritos().add(this);
 
     }
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Voce não esta matriculado em nenhum conteudo");
+        }
+    }
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        return this.conteudosInscritos.stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
